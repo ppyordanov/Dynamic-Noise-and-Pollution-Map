26c3c6f9f178a8e825c442a70c0b6cf2edfb76a5
ppyordanov <ppyordanov@yahoo.com>
2014-10-13 17:48:32 +0100

    spring RESTful web service [BASIC SERVER SET UP COMPLETE]

---
+++ src/main/java/com/springapp/mvc/Application.java
@@ -0,0 +1,17 @@
+package com.springapp.mvc;
+
+/**
+ * Created by Peter Yordanov on 13.10.2014 г..
+ */
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

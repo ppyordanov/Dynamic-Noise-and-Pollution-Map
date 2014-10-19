78959bdbd372134cacb3efb90dbe1219c0389a95
ppyordanov <ppyordanov@yahoo.com>
2014-10-19 18:39:57 +0100

    [MODELS] #12 Created stub model for user and implemented Device model

--- src/main/java/com/springapp/mvc/models/Device.java
+++ src/main/java/com/springapp/mvc/models/Device.java
@@ -4,4 +4,84 @@ package com.springapp.mvc.models;
  * Created by Peter Yordanov on 19.10.2014 г..
  */
 public class Device {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private String kit_version;
+    private String mac_address;
+
+    //default constructor
+    public Device(){
+
+        super();
+        this.id  = null;
+        this.title = null;
+        this.description = null;
+        this.kit_version = null;
+        this.mac_address = null;
+
+    }
+
+    //parameterized constructor
+    public Device(String title, String description, String kit_version, String mac_address){
+        this.id = null;
+        this.title = title;
+        this.description = description;
+        this.kit_version = kit_version;
+        this.mac_address = mac_address;
+    }
+
+    //copy constructor
+    public Device(Device d){
+        this.id = null;
+        this.title = d.getTitle();
+        this.description = d.getDescription();
+        this.kit_version = d.getKit_version();
+        this.mac_address = d.getMac_address();
+    }
+
+    public String getMac_address() {
+        return mac_address;
+    }
+
+    public void setMac_address(String mac_address) {
+        this.mac_address = mac_address;
+    }
+
+    public String getKit_version() {
+        return kit_version;
+    }
+
+    public void setKit_version(String kit_version) {
+        this.kit_version = kit_version;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString(){
+        return null;
+    }
 }
--- src/main/java/com/springapp/mvc/models/User.java
+++ src/main/java/com/springapp/mvc/models/User.java
@@ -4,4 +4,10 @@ package com.springapp.mvc.models;
  * Created by Peter Yordanov on 19.10.2014 г..
  */
 public class User {
+
+    //This class will be defined as a stub until user authentication has been implemented.
+
+    /*
+     */
+
 }

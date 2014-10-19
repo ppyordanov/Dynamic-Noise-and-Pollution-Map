9be9c2c9aeab8e29822a96a9cdede825810a5b4a
ppyordanov <ppyordanov@yahoo.com>
2014-10-19 05:01:32 +0100

    [MODELS] #12 created classes for models

--- src/main/java/com/springapp/mvc/models/DataReading.java
+++ src/main/java/com/springapp/mvc/models/DataReading.java
@@ -3,5 +3,5 @@ package com.springapp.mvc.models;
 /**
  * Created by Peter Yordanov on 14.10.2014 г..
  */
-public class SensorReading {
+public class DataReading {
 }
---
+++ src/main/java/com/springapp/mvc/models/User.java
@@ -0,0 +1,7 @@
+package com.springapp.mvc.models;
+
+/**
+ * Created by Peter Yordanov on 19.10.2014 г..
+ */
+public class User {
+}

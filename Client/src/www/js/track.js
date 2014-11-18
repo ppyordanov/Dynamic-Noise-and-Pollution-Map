051db40df48a7ac2d35555fbb786bc1fe8d84a2a
ppyordanov <ppyordanov@yahoo.com>
2014-11-18 19:18:49 +0000

    [TEST_DATA] restructure

--- Client/src/www/js/track.js
+++ Client/src/www/js/track.js
@@ -1,4 +1,6 @@
-
+var location_id = null;
+var route_data = [];
+var route_id = '';
 
 document.addEventListener("deviceready", function(){
 
@@ -10,10 +12,6 @@ document.addEventListener("deviceready", function(){
 
 });
 
-
-var location_id = null;
-var route_data = [];
-var route_id = '';
 
 $("#start").live('click',function(){
 
@@ -30,12 +28,17 @@ $("#start").live('click',function(){
         { enableHighAccuracy:true,frequency:30000}
     );
 
-        route_id = $("#route_id").val();
-        $("#route_id").hide();
-        $("#start").hide();
-        $("#clear").hide();
-        $("#status").html("tracking in progress");
-        $("#stop").show();
+    route_id = $("#route_id").val();
+    var time_now;
+    if(route_id ==''){
+        time_now = new Date().toLocaleString();
+        route_id = "Route " + time_now;
+    }
+
+
+    $("#clear").hide();
+    $("#status").html("tracking in progress");
+    $("#stop").show();
 
 });
 
@@ -47,14 +50,16 @@ $("#stop").live('click', function(){
     route_id=null;
     route_data=[];
 
-    $("#route_id").val("").show();
+    $("#route_id").val("");
     $("#status").html("completed tracking");
 
 });
 
 
 $("#clear").live('click', function(){
+
     window.localStorage.clear();
+
 });
 
 $('#history').live('pageshow', function () {
@@ -68,7 +73,7 @@ $('#history').live('pageshow', function () {
 
     for(i=0; i<route_number; i++){
         route = window.localStorage.key(i);
-        $("#list").append("<li><a href='#info' data-ajax='false'>" + route + "</a></li>");
+        $("#list").append("<li><a href='#route' data-ajax='false'>" + route + "</a></li>");
     }
     $("#list").listview('refresh');
 
@@ -76,6 +81,37 @@ $('#history').live('pageshow', function () {
 
 $("#list li a").live('click', function(){
 
-    $("#info").attr("route_id", $(this).text());
+    $("#route").attr("route_id", $(this).text());
+
+});
+
+$('#route').live('pageshow', function(){
+
+
+    var current_route_id = $(this).attr("route_id");
+    var current_route_data = window.localStorage.getItem(current_route_id);
+    current_route_data = JSON.parse(current_route_data);
+    var route_location = new google.maps.LatLng(current_route_data[0].coords.latitude, current_route_data[0].coords.longitude);
+    var myOptions = {
+        zoom: 15,
+        center: route_location,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    var route_coordinates = [];
+
+    for(i=0; i<data.length; i++){
+        route_coordinates.push(new google.maps.LatLng(current_route_data[i].coords.latitude, current_route_data[i].coords.longitude));
+    }
+
+    var route_display = new google.maps.Polyline({
+        path: trackCoords,
+        strokeColor: "#999999",
+        strokeOpacity: 1.0,
+        strokeWeight: 3
+    });
+
+    route_display.setMap(map);
+
 
 });
